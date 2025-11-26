94a0cd8e0319f57903399182fb0d7d7713db9674
Nenii24 <erandenigomez12@gmail.com>
2025-11-26 10:01:57 -0700

    El menu y la de ver resgitro a medias

--- proyectoFinalHotel/src/proyectoFinalHotel/DemoFrame.java
+++ proyectoFinalHotel/src/proyectoFinalHotel/DemoFrame.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
 
 public class DemoFrame extends JFrame {
 
@@ -47,6 +48,38 @@ public class DemoFrame extends JFrame {
 		Titulo.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 17));
 		Titulo.setBounds(293, 58, 152, 12);
 		contentPane.add(Titulo);
+		
+		JButton verRegistro = new JButton("Ver registro huéspedes");
+		verRegistro.setBounds(105, 280, 152, 41);
+		contentPane.add(verRegistro);
+		
+		JButton buscarHuesped = new JButton("Buscar huésped");
+		buscarHuesped.setBounds(105, 331, 152, 41);
+		contentPane.add(buscarHuesped);
+		
+		JButton reservarHabitacion = new JButton("Reservar habitacion");
+		reservarHabitacion.setBounds(105, 382, 152, 41);
+		contentPane.add(reservarHabitacion);
+		
+		JButton checkIn = new JButton("Check in");
+		checkIn.setBounds(105, 433, 152, 41);
+		contentPane.add(checkIn);
+		
+		JButton checkOut = new JButton("Check out");
+		checkOut.setBounds(441, 280, 152, 41);
+		contentPane.add(checkOut);
+		
+		JButton verDisponibilidad = new JButton("Ver disponibilidad");
+		verDisponibilidad.setBounds(441, 331, 152, 41);
+		contentPane.add(verDisponibilidad);
+		
+		JButton verIngresos = new JButton("Ver ingresos");
+		verIngresos.setBounds(441, 382, 152, 41);
+		contentPane.add(verIngresos);
+		
+		JButton buscarHuesped_3 = new JButton("Buscar huésped");
+		buscarHuesped_3.setBounds(441, 433, 152, 41);
+		contentPane.add(buscarHuesped_3);
 
 	}
 }
