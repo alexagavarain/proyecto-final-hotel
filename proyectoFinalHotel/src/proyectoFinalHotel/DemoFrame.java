47fca2db9853e7006da5395bc9034658ad2c47d1
Nenii24 <erandenigomez12@gmail.com>
2025-11-26 09:03:37 -0700

    Todos los cambios

---
+++ proyectoFinalHotel/src/proyectoFinalHotel/DemoFrame.java
@@ -0,0 +1,52 @@
+package proyectoFinalHotel;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class DemoFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DemoFrame frame = new DemoFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DemoFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 730, 556);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 128));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel Titulo = new JLabel("Hotel \"El buen descanso\"");
+		Titulo.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 17));
+		Titulo.setBounds(293, 58, 152, 12);
+		contentPane.add(Titulo);
+
+	}
+}
